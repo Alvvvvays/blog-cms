ad7e49716a7a76cfcfb7be371393dde5f671670f
Alvvvvays <56679385+Alvvvvays@users.noreply.github.com>
2021-02-06 10:52:26 +0000

    Fix memory leak in file upload

--- server.js
+++ server.js
@@ -16,3 +16,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Added: new utility function
 # Fix: handle None values properly
 # Updated logic for better readability
+// Updated: improve performance
