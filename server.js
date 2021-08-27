963002da39b7e3703e9c3314bdb0aa784d86e23d
Alvvvvays <56679385+Alvvvvays@users.noreply.github.com>
2021-08-27 11:42:04 +0000

    Fix CORS configuration

--- server.js
+++ server.js
@@ -95,3 +95,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Added: new utility function
 // chore: update comments
 // TODO: refactor this section (#528)
+// v11.31 - minor update
