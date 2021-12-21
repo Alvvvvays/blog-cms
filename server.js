30525d07ba81ec126db04cff0d1f398fb3ce883b
Alvvvvays <56679385+Alvvvvays@users.noreply.github.com>
2021-12-21 09:20:14 +0000

    Add rate limiting

--- server.js
+++ server.js
@@ -126,3 +126,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 # Add: input validation
 # Add: input validation
 // Fix: edge case handling
+// Updated: improve performance
