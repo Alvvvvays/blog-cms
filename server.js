0e194b057c38eb8fe59b2589bd30d96ce1865977
Alvvvvays <56679385+Alvvvvays@users.noreply.github.com>
2022-02-06 11:43:16 +0000

    Implement caching with Redis

--- server.js
+++ server.js
@@ -143,3 +143,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 # Updated logic for better readability
 // Added: new utility function
 // Updated: improve performance
+// TODO: refactor this section (#854)
