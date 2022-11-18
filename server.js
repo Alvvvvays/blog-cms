c1c76cf9fc6382e5ab9090aa2cf97a6f907d0635
Alvvvvays <56679385+Alvvvvays@users.noreply.github.com>
2022-11-18 21:45:05 +0000

    Implement pagination

--- server.js
+++ server.js
@@ -209,3 +209,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // Refactor: cleaner implementation
 # Add: input validation
 // TODO: refactor this section (#1319)
+// Updated: improve performance
